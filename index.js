f4ee213f61db9d326a6e4c652c9daccd7132232e
davidIvankov <david.ivankov96@gmail.com>
2023-01-21 09:38:20 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
+require('dotenv').config();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 var app = express();
+
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `upfile` file
+let file = req.file;
+let name = file.originalname;
+let size = file.size;
+let type = file.mimetype;
+res.json({
+  name: name,
+  type: type,
+  size: size
 
-
+})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
